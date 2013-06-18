c47555ef2506f35a4ae61c8f092796c51756c875
Alexander Godlewski <alex@implode.ca>
2013-06-17 20:10:12 -0400

    Added a license, updated readme, added plugin

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ dragcheck
 =========
 
 A jQuery javascript drag checking plugin for checkboxes
+
+Allows clicking and then dragging across checkboxes to check/uncheck them
+If you skip a bunch and go to a later checkbox it will fill in the missing ones in between
+
+To use in a basic case:
+
+    $('selector').dragcheck({
+        container: 'tr',
+        onSelect: function(obj, state) {
+            obj.attr('checked', state);
+        }
+    });
+
+Options:
+
+- container: The element that contains the checkboxes and has other sibling container elements that contain checkboxes
+For example if you do:
+
+
+    $('table tbody').dragcheck({
+        container: 'tr',
+        onSelect: function(obj, state) {
+            obj.attr('checked', state);
+        }
+    });
+
+Then the checkboxes in the trs will be affected by dragcheck
+
+- onSelect: When the checkbox is selected to be checked, this method will be called. You can choose to check the box,
+or run a condition and optionally check the box or do anything you want
+
+
+The overall goal of this plugin is to provide an easy way for the user to click and drag down to check checkboxes instead
+of having to slowly click each one. The original use was for checkboxes in the first column of a table with tabular data
---
+++ dragcheck.js
@@ -0,0 +1,63 @@
+﻿(function ($) {
+    $.fn.dragcheck = function (options) {
+        
+        var opts = $.extend({}, $.fn.dragcheck.defaults, options);
+
+        return this.each(function () {
+            var $this = $(this);
+
+            var enabled = false;
+            var state = false;
+            var startIndex = null;
+            
+            // meta data plugin support
+            var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
+
+            $(':checkbox', $this)
+                .mousedown(function () {
+                    var $obj = $(this);
+                    enabled = true;
+                    state = !$obj.is(':checked');
+                    startIndex = (o.container ? $obj.parents(o.container).index() : $obj.index);
+
+                    $obj.one('mouseout', function () {
+                        o.onSelect($(this), state);
+                    });
+                })
+                .mouseover(function () {
+                    if (enabled) {
+                        var $obj = $(this);
+                        var sourceRow = (o.container ? $obj.parents(o.container) : $obj);
+                        var targetRow = (o.container ? $obj.parents(o.container).parent().children(o.container).eq(startIndex) : $obj.parent().children(':checkbox').eq(startIndex));
+
+                        var endIndex = sourceRow.index();
+                        if (endIndex > startIndex) {
+                            var prevelements = sourceRow.prevUntil(targetRow, o.container || ':checkbox');
+                            o.onSelect((o.container ? targetRow.find(':checkbox') : targetRow), state);
+                            o.onSelect((o.container ? sourceRow.find(':checkbox') : sourceRow), state);
+                            $.each((o.container ? prevelements.find(':checkbox') : prevelements), function() {
+                                o.onSelect($(this), state);
+                            });
+                        }
+                        else if (endIndex < startIndex) {
+                            var nextelements = sourceRow.nextUntil(targetRow, o.container || ':checkbox');
+                            o.onSelect((o.container ? targetRow.find(':checkbox') : targetRow), state);
+                            o.onSelect((o.container ? sourceRow.find(':checkbox') : sourceRow), state);
+                            $.each((o.container ? nextelements.find(':checkbox') : nextelements), function () {
+                                o.onSelect($(this), state);
+                            });
+                        }
+                    }
+                });
+
+            $('html').mouseup(function () {
+                enabled = false;
+            });
+        });
+    };
+    
+    $.fn.dragcheck.defaults = {
+        container: null,
+        onSelect: function (obj, state) { }
+    };
+})(jQuery);
